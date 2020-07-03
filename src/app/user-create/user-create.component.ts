1543244f087145bae8d57187315297c781db6d35
Muamer Bajrami <bajrami.muamer@gmail.com>
2020-07-03 19:18:07 +0200

    Bug fix

--- src/app/user-create/user-create.component.ts
+++ src/app/user-create/user-create.component.ts
@@ -1,9 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {UserModel} from '../api/users/user.model';
+import {FormBuilder, FormGroup, ValidationErrors, Validators} from '@angular/forms';
 import {UserService} from '../api/users/users.service';
-import {confirmPasswordValidator} from '../shared/validators/confirmPassword.validator';
-import {ProfileModel} from '../api/profile/profile.model';
+import {confirmPasswordValidator, getFormValidationErrors} from '../shared/validators/confirmPassword.validator';
 
 @Component({
   selector: 'app-user-create',
@@ -14,45 +12,57 @@ export class UserCreateComponent implements OnInit {
 
   myForm: FormGroup;
 
-  constructor(private fb: FormBuilder, private userService: UserService) { }
+  constructor(private fb: FormBuilder, private userService: UserService) {
+  }
 
-  createUser(){
-    let newUser = this.myForm.value;
-    newUser.profileUrl = "https://upload.wikimedia.org/wikipedia/commons/6/6d/Windows_Settings_app_icon.png";
-    this.userService.createUser(newUser).subscribe(res => console.log("Res: ", res), err => console.error("Err: ", err));
+  // function name was same as method name of user service.
+  async createNewUser() {
+    this.myForm.patchValue({
+      profileUrl: 'https://upload.wikimedia.org/wikipedia/commons/6/6d/Windows_Settings_app_icon.png',
+      full_name: `${this.myForm.value.name} ${this.myForm.value.surname}`
+    });
+    if (this.myForm.valid) {
+      await this.userService.createUser(this.myForm.value).subscribe(res => console.log('Res: ', res), err => console.error('Err: ', err));
+    } else {
+      getFormValidationErrors(this.myForm);
+    }
   }
 
   onSelectFile(event) { // called each time file input changes
     if (event.target.files && event.target.files[0]) {
-      var reader = new FileReader();
-
+      const reader = new FileReader();
       reader.readAsDataURL(event.target.files[0]); // read file as data url
-
       reader.onload = (event) => { // called once readAsDataURL is completed
-        this.myForm.patchValue({ profile: event.target.result.toString()});
-      }
+        this.myForm.patchValue({profile: event.target.result.toString()});
+      };
     }
   }
 
-  buildForm(){
+  buildForm() {
     this.myForm = this.fb.group(
       {
-        name: ['MOT', Validators.required], 
+        name: ['MOT', Validators.required],
         role: ['staff', [Validators.required]],
-        email: ['l@l.l', [Validators.required, Validators.email]], 
-
-        phoneNumber: ['3213213', [Validators.required, Validators.pattern("^[0-9]*$")]],
+        email: ['l@l.l', [Validators.required, Validators.email]],
+        phoneNumber: ['3213213', [Validators.required, Validators.pattern('^[0-9]*$')]],
         surname: ['hey', Validators.required],
-        profileUrl: ['null', [Validators.required]],
-      
-        password: ['', [Validators.required, confirmPasswordValidator]], 
-        confirmPassword: ['', [Validators.required, confirmPasswordValidator]],
+        full_name: [''], // if you want to have validators for this field you need to monitor your input fields with method onKeyup... so you can know name or surname is added then join them.
+        profileUrl: [''],
+        password: ['', [Validators.required]],
+        password_confirmation: ['', [Validators.required]],
         country: ['Maceee', [Validators.required]],
         is_active_account: [true, []],
+      }, {
+        validator: confirmPasswordValidator('password', 'password_confirmation')
       }
     );
   }
- 
+
+  // Abstract Control
+  get f() {
+    return this.myForm.controls;
+  }
+
   ngOnInit(): void {
     this.buildForm();
   }
